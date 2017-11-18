78f5b9e7c00a7e8a1f5451b2141aea3392235450
ITCoderWang <15076509228@sina.cn>
2017-11-18 12:04:46 +0800

    Initial Commit

---
+++ WWDesignPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWDesignPattern
+//
+//  Created by 王永旺永旺 on 2017/11/18.
+//  Copyright © 2017年 ITCoderW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
